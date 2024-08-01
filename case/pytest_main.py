04073a4efbe440bcdede4d225565105d1a465845
DESKTOP-68T8R9R\Administrator <1419310155@qq.com>
2024-08-01 21:10:17 +0800

    2024/08/01:修改生成报告代码

--- case/pytest_main.py
+++ case/pytest_main.py
@@ -1,4 +1,12 @@
 import pytest
+#
+# if __name__ == '__main__':
+#     pytest.main(['-vs', 'DriverTools.py'])
 
-if __name__ == '__main__':
-    pytest.main(['-vs', 'DriverTools.py'])
+
+# 添加命令行参数 --html=report.html 和 --html=report.html --self-contained-html
+if __name__ == "__main__":
+    pytest.main(["DriverTools.py",   # 测试用例
+                 "--html=../report/report.html",   # 生成测试报告 生成assert存放的css文件和html文件
+                 "--self-contained-html",  # 把css样式合并到html里 仅生成html文件
+                 ])
